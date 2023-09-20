100e6f79a42cb69bdfd5b12c5e5be2dd459ade8c
Renie Adjani <renieadjani07@gmail.com>
2023-09-20 17:32:48 +0800

    update cors

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://bubble-tea-mern.netlify.app",
     credentials: true,
   })
 );
